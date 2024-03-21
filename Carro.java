90f76b5476e79872354adbda08bb27efe5f4b53c
Gustavo dos Santos Silva <103334856+GuSilva12@users.noreply.github.com>
2024-03-21 16:15:00 -0300

    Update Carro.java
    Adição de comentários ao código.

--- Carro.java
+++ Carro.java
@@ -1,22 +1,25 @@
-package ex01_extra;
+package lab01_02_extra;
 
 public class Carro {
-	
+	//definição de variáveis, privadas para que acesso seja permitido apenas se constar requisição no devido código
 	private String modelo;
 	private int ano;
 	private double tanque;
 	
+	//constructor, 'puxa' os valores das variaveis das instancias na classe TestePosto, para invocar quando solicitado nessa classe
 	public Carro (String aModelo, int aAno, double aTanque) {
 		this.modelo = aModelo;
 		this.ano = aAno;
 		this.tanque = aTanque;
 		
 	}
-	//
+	
+	//get retorna o valor do respectivo campo
 	public String getModelo() {
 		return modelo;
 	}
 	
+	//set define o valor do campo com o valor fornecido
 	public void setModelo (String modelo) {
 		this.modelo = modelo; 
 	}
